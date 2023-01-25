7460f6c0a04db8683fb523adadfc26d73828d128
Tanner Byers <naultic@amazon.com>
2023-01-25 13:04:59 -0500

    added tailwind with example UI

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect} from "react";
-import { Amplify, API } from 'aws-amplify'
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { Amplify, API } from "aws-amplify";
 import awsExports from "./aws-exports";
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(awsExports);
 
-  const apiName = 'crudapi';
-  const path = '/subscription';
+const apiName = "crudapi";
+const path = "/subscription";
 
- API.get(apiName, path);
-
+API.get(apiName, path);
 
 function App({ signOut, user }) {
-
+  console.log({ user });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex h-screen">
+        <div className="w-1/5 bg-gray-500 flex justify-center">
+          <ul className="flex flex-col justify-start h-screen text-center">
+            <li className="mr-6 m-3">
+              <a className="text-white hover:text-gray-800" href="#">
+                Home
+              </a>
+            </li>
+            <li className="mr-6 m-3">
+              <a className="text-white hover:text-gray-800" href="#">
+                Send Notifications
+              </a>
+            </li>
+            <li className="mr-6 m-3">
+              <a className="text-white hover:text-gray-800" href="#">
+                Check Subscribers
+              </a>
+            </li>
+            <li className="mr-6 m-3">
+              <a className="text-white hover:text-gray-800" href="#">
+                Analytics
+              </a>
+            </li>
+            <li className="mr-6 m-3 mt-6">
+              <a className="text-white hover:text-gray-800" onClick={signOut}>
+                Sign Out
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-3/5 bg-gray-400 "></div>
+        <div className="w-1/5 bg-gray-500 flex justify-center ">
+          <p>Hello {user.username}</p>
+        </div>
+      </div>
     </div>
   );
 }
